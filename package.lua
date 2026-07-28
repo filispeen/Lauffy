2e2fb84dc9eb2947fa12e8efc6400d6622d23c0b
filispeen <illayfilisp@gmail.com>
2026-07-29 00:07:05 +0300

    Initialized repo with current package needs

---
+++ package.lua
@@ -0,0 +1,18 @@
+  return {
+    name = "Lauffy",
+    version = "0.0.1",
+    description = "Discord music bot written in lua using disocrd.lua",
+    tags = { "discord", "music", "discord-lua" },
+    license = "MIT",
+    author = { name = "filispeen", email = "illayfilisp@gmail.com" },
+    homepage = "https://github.com/filispeen/Lauffy",
+    dependencies = {
+      "filispeen/discord.lua@0.9.1",
+      "filispeen/lavalink.lua@0.2.6"
+    },
+    files = {
+      "main.lua",
+      "!test*"
+    }
+  }
+  
